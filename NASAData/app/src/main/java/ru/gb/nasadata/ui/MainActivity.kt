cde8fc1161851ea5cd0c1d6f02072e4b66e4db6d
Tatiana Zotova <zotova-tv@outlook.com>
2022-05-15 19:37:31 +0300

    lesson2

--- NASAData/app/src/main/java/ru/gb/nasadata/ui/MainActivity.kt
+++ NASAData/app/src/main/java/ru/gb/nasadata/ui/MainActivity.kt
@@ -1,11 +1,22 @@
 package ru.gb.nasadata.ui
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import ru.gb.nasadata.R
 import ru.gb.nasadata.ui.picture.PictureOfTheDayFragment
+import ru.gb.nasadata.ui.settings.SettingsFragment
 
-class MainActivity : AppCompatActivity() {
+const val SETTINGS_PREFS = "SETTINGS_PREFS"
+const val NIGHT_MODE_TAG = "NIGHT_MODE_TAG"
+
+class MainActivity :
+    AppCompatActivity(),
+    SettingsFragment.SwitchNightModeListener {
+
+    private var prefs: SharedPreferences? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +25,27 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, PictureOfTheDayFragment.newInstance())
                 .commitNow()
+        }
+
+        prefs = getSharedPreferences(SETTINGS_PREFS, Context.MODE_PRIVATE)
+        val nightModeIsChecked = prefs?.getBoolean(NIGHT_MODE_TAG, false) ?: false
+        switchNightMode(nightModeIsChecked)
+    }
+
+    override fun onSwitchNightMode(isChecked: Boolean) {
+        prefs?.let {
+            it.edit().also {editor ->
+                editor.putBoolean(NIGHT_MODE_TAG, isChecked)
+                editor.apply()
+            }
+        }
+        switchNightMode(isChecked)
+    }
+
+    private fun switchNightMode(isChecked: Boolean){
+        when (isChecked) {
+            true -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            false -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         }
     }
 }
---
+++ NASAData/app/src/main/java/ru/gb/nasadata/ui/settings/SettingsFragment.kt
@@ -0,0 +1,57 @@
+package ru.gb.nasadata.ui.settings
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import ru.gb.nasadata.databinding.FragmentSettingsBinding
+import ru.gb.nasadata.ui.NIGHT_MODE_TAG
+import ru.gb.nasadata.ui.SETTINGS_PREFS
+
+class SettingsFragment : Fragment() {
+
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+    private var switchNightModeListener: SwitchNightModeListener? = null
+    private var prefs: SharedPreferences? = null
+    private var nightModeIsChecked = false
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        activity?.let {
+            switchNightModeListener = it as SwitchNightModeListener
+            prefs = it.getSharedPreferences(SETTINGS_PREFS, Context.MODE_PRIVATE).also {prefs ->
+                nightModeIsChecked = prefs.getBoolean(NIGHT_MODE_TAG, false)
+            }
+        }
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding.switchNightMode){
+            isChecked = nightModeIsChecked
+            setOnCheckedChangeListener{ _, isChecked ->
+                switchNightModeListener?.onSwitchNightMode(isChecked)
+
+                Toast.makeText(context, if(isChecked) "Night theme activated" else "Night theme deactivated", Toast.LENGTH_SHORT).apply {
+                    setGravity(Gravity.BOTTOM, 0, 250)
+                    show()
+                }
+            }
+        }
+    }
+
+    interface SwitchNightModeListener {
+        fun onSwitchNightMode(isChecked: Boolean)
+    }
+}
